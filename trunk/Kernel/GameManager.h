a07eb78a1ebe477c7d3c7e8ddb8e348b80f0f04f
littlebuddy <littlebuddy@4e25fa27-4b24-0410-b14f-b2fbb519f7c8>
2005-11-15 22:36:09 +0000

    + Added IGraphics* GetGraphicsI(); for retrieving the graphics interface.

--- trunk/Kernel/GameManager.h
+++ trunk/Kernel/GameManager.h
@@ -161,6 +161,7 @@ public:
 	const IScript* GetScript(int idx) const;
 	const IScript* GetScript(CSprite *pSprite) const;
 	ISoundManager* GetSoundManager() const;
+	IGraphics* GetGraphicsI() const;
 
 	// Object factory methods:
 	CSprite *ReferSprite(LPCSTR szName, _spt_type sptType, LPCSTR szFile_, int nLine_);  //!< Gets or makes a reference to a sprite.
